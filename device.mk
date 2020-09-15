8e779cdbbe442a2a5e83aaf2dc4f0898c78558e3
Navin <karupal2002@gmail.com>
2020-09-15 15:51:07 +0000

    Revert "raphael: Exclude lineage recorder"
    This reverts commit fffff3747e753473ce634fdb4ae90d21b4035b6f.

--- device.mk
+++ device.mk
@@ -336,10 +336,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
 
-# Remove packages
-PRODUCT_PACKAGES += \
-    RemovePackages
-
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
